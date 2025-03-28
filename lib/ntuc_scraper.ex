ba306e3f0b5445d3a6f933d646ff9808b2ec6aa5
Gerald Teo <geraldteo7@gmail.com>
2025-03-29 00:17:27 +0800

    Food cupboard

--- lib/ntuc_scraper.ex
+++ lib/ntuc_scraper.ex
@@ -2,11 +2,11 @@ defmodule NtucScraper do
   use Crawly.Spider
 
   @impl Crawly.Spider
-  def base_url(), do: "https://www.fairprice.com.sg/category/bakery"
+  def base_url(), do: "https://www.fairprice.com.sg/category/food-cupboard-6"
 
   @impl Crawly.Spider
   def init() do
-    [start_urls: ["https://www.fairprice.com.sg/category/bakery"]]
+    [start_urls: ["https://www.fairprice.com.sg/category/food-cupboard-6"]]
   end
 
   @impl Crawly.Spider
