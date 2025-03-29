d96fa2eba0e8ad0775ff273a68cc8dfe731c1b94
Gerald Teo <geraldteo7@gmail.com>
2025-03-29 18:51:59 +0800

    Add frozen

--- lib/ntuc_price_is_right/ntuc_scraper.ex
+++ lib/ntuc_price_is_right/ntuc_scraper.ex
@@ -2,11 +2,11 @@ defmodule NtucPriceIsRight.NtucScraper do
   use Crawly.Spider
 
   @impl Crawly.Spider
-  def base_url(), do: "https://www.fairprice.com.sg/category/fruits-vegetables"
+  def base_url(), do: "https://www.fairprice.com.sg/category/frozen"
 
   @impl Crawly.Spider
   def init() do
-    [start_urls: ["https://www.fairprice.com.sg/category/fruits-vegetables"]]
+    [start_urls: ["https://www.fairprice.com.sg/category/frozen"]]
   end
 
   @impl Crawly.Spider
