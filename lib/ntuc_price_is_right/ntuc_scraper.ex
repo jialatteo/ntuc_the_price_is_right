df447379b68cfe78ca5bab6550f1a5e44f35c7f5
Gerald Teo <geraldteo7@gmail.com>
2025-03-29 18:53:16 +0800

    Add drinks

--- lib/ntuc_price_is_right/ntuc_scraper.ex
+++ lib/ntuc_price_is_right/ntuc_scraper.ex
@@ -2,11 +2,11 @@ defmodule NtucPriceIsRight.NtucScraper do
   use Crawly.Spider
 
   @impl Crawly.Spider
-  def base_url(), do: "https://www.fairprice.com.sg/category/frozen"
+  def base_url(), do: "https://www.fairprice.com.sg/category/drinks"
 
   @impl Crawly.Spider
   def init() do
-    [start_urls: ["https://www.fairprice.com.sg/category/frozen"]]
+    [start_urls: ["https://www.fairprice.com.sg/category/drinks"]]
   end
 
   @impl Crawly.Spider
