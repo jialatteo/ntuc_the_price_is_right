c7ec23fe93ec0857ee8caceb1ab21dbc48f9f511
Gerald Teo <geraldteo7@gmail.com>
2025-04-02 00:02:03 +0800

    Add previous submission table

--- lib/ntuc_price_is_right_web/live/home_live.ex
+++ lib/ntuc_price_is_right_web/live/home_live.ex
@@ -4,7 +4,14 @@ defmodule NtucPriceIsRightWeb.HomeLive do
   alias NtucPriceIsRight.GuessedPrice
 
   defmodule Submission do
-    defstruct [:id, :product_name, :image, :actual_price, :guessed_price, :quantity, :is_correct]
+    defstruct [
+      :id,
+      :product_name,
+      :image,
+      :actual_price,
+      :guessed_price,
+      :quantity
+    ]
 
     @type t :: %__MODULE__{
             id: String.t(),
@@ -12,8 +19,7 @@ defmodule NtucPriceIsRightWeb.HomeLive do
             image: String.t(),
             quantity: String.t(),
             actual_price: float(),
-            guessed_price: float(),
-            is_correct: boolean()
+            guessed_price: float()
           }
   end
 
@@ -23,7 +29,8 @@ defmodule NtucPriceIsRightWeb.HomeLive do
 
     {:ok,
      socket
-     |> assign(:number_of_correct, 0)
+     |> assign(:score, 0)
+     |> assign(:correct_streak, 0)
      |> assign(:product, product)
      |> stream(:submissions, [])
      |> assign(:guessed_price_form, guessed_price_form)}
@@ -78,31 +85,17 @@ defmodule NtucPriceIsRightWeb.HomeLive do
       </div>
     </.form>
 
-    <p>Past Submissions</p>
+    <p>Score {@score}</p>
 
-    <%!--
-    <div class="flex justify-between">
-      <p>Product</p>
+    <p class="font-bold text-2xl mt-8 mb-3">Previous Submissions</p>
 
-      <p>Actual Price</p>
-
-      <p>Guessed Price</p>
-
-      <p>Status</p>
-    </div>
-
-    <div id="submissionss" phx-update="stream">
-      <div :for={{dom_id, room} <- @streams.submissions} class="flex">
-
-      </div>
-    </div> --%>
-    <.table table_class="mt-0" id="submissions" rows={@streams.submissions}>
+    <.table table_class="w-full" id="submissions" rows={@streams.submissions}>
       <:col :let={{dom_id, submission}} label="Product">
-        <div class="flex max-w-40 sm:max-w-full sm:flex-row flex-col items-center text-sm">
+        <div class="flex sm:flex-row flex-col sm:items-center text-sm">
           <img class="size-12" src={submission.image} alt="submission.product_name" />
-          <p class="ml-1 font-light">
+          <span class="sm:ml-1">
             {submission.product_name} ({submission.quantity})
-          </p>
+          </span>
         </div>
       </:col>
       
@@ -114,7 +107,16 @@ defmodule NtucPriceIsRightWeb.HomeLive do
       
       <:col :let={{dom_id, submission}} label="Guessed Price">
         <p class="text-xl font-semibold">
-          ${submission.guessed_price} {if submission.is_correct, do: "✅", else: "❌"}
+          ${submission.guessed_price}
+        </p>
+      </:col>
+      
+      <:col :let={{dom_id, submission}} label="Points">
+        <p class={[
+          "text-2xl font-bold",
+          if(@correct_streak > 0, do: "text-green-500", else: "text-gray-400")
+        ]}>
+          +{min(@correct_streak, 5)}
         </p>
       </:col>
     </.table>
@@ -129,12 +131,14 @@ defmodule NtucPriceIsRightWeb.HomeLive do
       actual_price = socket.assigns.product.price
 
       is_correct_guess =
-        0.9 * actual_price <= guessed_price and guessed_price <= 1.1 * actual_price
+        0.8 * actual_price <= guessed_price and guessed_price <= 1.2 * actual_price
 
-      number_of_correct =
+      correct_streak =
         if is_correct_guess,
-          do: socket.assigns.number_of_correct + 1,
-          else: socket.assigns.number_of_correct
+          do: socket.assigns.correct_streak + 1,
+          else: 0
+
+      score = socket.assigns.score + min(correct_streak, 5)
 
       submission = %Submission{
         id: Ecto.UUID.generate(),
@@ -142,8 +146,7 @@ defmodule NtucPriceIsRightWeb.HomeLive do
         product_name: socket.assigns.product.title,
         quantity: socket.assigns.product.quantity,
         actual_price: :erlang.float_to_binary(actual_price, decimals: 2),
-        guessed_price: format_guessed_price(price),
-        is_correct: is_correct_guess
+        guessed_price: format_guessed_price(price)
       }
 
       {:noreply,
@@ -151,7 +154,8 @@ defmodule NtucPriceIsRightWeb.HomeLive do
        |> stream_insert(:submissions, submission, at: 0)
        |> assign(:guessed_price_form, to_form(GuessedPrice.changeset(%GuessedPrice{}, %{})))
        |> assign(:is_correct_guess, is_correct_guess)
-       |> assign(:number_of_correct, number_of_correct)
+       |> assign(:correct_streak, correct_streak)
+       |> assign(:score, score)
        |> assign(:product, Products.get_random_product())}
     else
       {:noreply,
